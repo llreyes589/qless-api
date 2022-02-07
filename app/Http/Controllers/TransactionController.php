357f2a40badbfcc79580a18fd9503372317b7199
Lester Lou Reyes <clarenista@gmail.com>
2022-02-07 22:22:23 +0800

    update

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -11,7 +11,7 @@ class TransactionController extends Controller
     public function create(){
         $card = Card::with('transactions')->find(\request()->card_id);
         $total_load = $card->load - \request()->fare;
-        $card->update(['load' => $total_load]);
+        $card->update(['load' => $total_load, 'expires_at' => $card->updated_at->add(5, 'year')]);
         $card->transactions()->create(\request()->all());
         return $card->load('transactions');
     }
